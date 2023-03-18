2efe1c7d58e165c0e131aea2cb31afb32c73c63d
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2023-03-18 02:52:03 +0200

    Select Features and Prepare Data

--- Road/source/constants.py
+++ Road/source/constants.py
@@ -5,3 +5,9 @@ CLASS = 5
 
 PLEASE WRITE CONSTANT VARIABLE NAME ALWAYS CAPITALIZED
 """
+COLUMNS_TO_DELETE = ['_', 'peer_id', 'out', 'mentioned',
+    'media_unread', 'silent', 'post', 'from_scheduled', 'legacy',
+    'edit_hide', 'pinned', 'noforwards',
+    'via_bot_id', 'media', 'reply_markup', 'views',
+    'forwards', 'edit_date', 'post_author', 'grouped_id',
+    'reactions', 'restriction_reason', 'ttl_period', 'fwd_from', 'entities', 'replies']
---
+++ Road/source/features/feature_selected.py
@@ -0,0 +1,62 @@
+# Import constant values
+from source.constants import COLUMNS_TO_DELETE
+# Necessary libraries
+import pandas as pd
+import ast
+import sys
+import os
+# Reset the path to the parent folder to import any mudule we want
+target = os.path.abspath(__file__)
+while (target.split("\\")[-1] != "Road"):
+    target = os.path.dirname(target)
+sys.path.append(target)
+
+
+def delete_unnecessery_columns(data: pd.DataFrame) -> None:
+    """Remove all unnecessary columns except id, date, message,
+    from_id and reply_to columns.
+
+    Args:
+        data (pd.DataFrame): The dataframe that will be modify.
+
+    Returns:
+        None
+    """
+    data.drop(columns=COLUMNS_TO_DELETE, inplace=True)
+
+
+def extract_dates(date: pd.Series) -> None:
+    """Extract year, month, day, hour, minute, and second values
+    from the date series.
+
+    Args:
+        date (pd.Series): The date column to turn it into useful columns
+
+    Returns:
+        None
+    """
+    year = pd.to_datetime(date).dt.year
+    month = pd.to_datetime(date).dt.month
+    day = pd.to_datetime(date).dt.day
+    hour = pd.to_datetime(date).dt.hour
+    minute = pd.to_datetime(date).dt.minute
+    second = pd.to_datetime(date).dt.second
+
+    return (year, month, day, hour, minute, second)
+
+
+def extract_id(json_string: str, target_id: str) -> int:
+    """Extracts an ID from a JSON string.
+
+    Args:
+        json_string: A JSON string containing one or more IDs.
+        target_it: The index of the desired ID in the JSON string.
+
+    Returns:
+        (int): The extracted ID, or -1 if the input is null or not a string.
+    """
+    if isinstance(json_string, str):
+        # Get the target ID after convert the string to a dictionary
+        return ast.literal_eval(json_string.replace("'", "\""))[target_id]
+    elif pd.isna(json_string):
+        return -1
